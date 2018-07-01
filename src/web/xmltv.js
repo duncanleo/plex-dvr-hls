dd00e8b7449ac4297482cdac9d522af262ad1acb
Duncan Leo <duncanleo97@gmail.com>
2018-07-01 17:05:33 +0800

    generate hour-long programmes for xmltv

--- src/web/xmltv.js
+++ src/web/xmltv.js
@@ -5,6 +5,7 @@ const dateFormat = require('dateformat');
 const channels = require('../../channels.json');
 
 const xmltv = (req, res) => {
+  const today = new Date();
   res.send(
     Mustache.render(
       fs.readFileSync('templates/xmltv.mustache', { encoding: 'utf8' }),
@@ -12,8 +13,19 @@ const xmltv = (req, res) => {
         channels: channels.map((c, index) => ({
           id: index + 1,
           name: c.name,
-          today: dateFormat(new Date(), 'yyyymmdd'),
         })),
+        programmes: [...Array(24).keys()].map((hour) => {
+          today.setHours(hour);
+          today.setMinutes(0);
+          today.setSeconds(0);
+          const dateTimeStart = dateFormat(today, 'yyyymmddHHmmss o');
+          const hourStr = dateFormat(today, 'htt');
+          today.setHours(hour + 1);
+          today.setMinutes(59);
+          today.setSeconds(59);
+          const dateTimeEnd = dateFormat(today, 'yyyymmddHHmmss o');
+          return { hourStr, dateTimeStart, dateTimeEnd };
+        }),
       },
     ),
   );
