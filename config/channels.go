4ddab4318002f72a319e7b519bae0c4b3f4c5c6b
e1z0 <7213361+e1z0@users.noreply.github.com>
2024-04-20 11:19:46 +0300

    feat: Add support for loading m3u playlists at runtime (#27)

--- config/channels.go
+++ config/channels.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"errors"
+	m3uparser "github.com/pawanpaudel93/go-m3u-parser/m3uparser"
 )
 
 type ProxyConfig struct {
@@ -26,7 +28,37 @@ var (
 	Channels []Channel
 )
 
+func LoadChannelsFromPl(location string) error {
+    var userAgent = os.Getenv("UA")
+    if len(userAgent) == 0 {
+      userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+	}
+	parser := m3uparser.M3uParser{UserAgent: userAgent, Timeout: 60}
+	parser.ParseM3u(location,true,true)
+	streams := parser.GetStreamsSlice()
+	if len(streams) > 0 {
+	    for _, st := range streams {
+			ch := Channel{Name: st["title"].(string), URL: st["url"].(string), UserAgent: &userAgent}
+			Channels = append(Channels,ch)
+		} 
+		if len(Channels) > 0 {
+			return nil
+		}
+	}
+	return errors.New("No streams in playlist")
+}
+
 func init() {
+    var playlist = os.Getenv("PLAYLIST")
+	if len(playlist) > 0 {
+	    err := LoadChannelsFromPl(playlist)	
+	    if err == nil {
+			return
+		} else {
+		    log.Printf("Provided m3u playlist error: %s\n",err)
+		}
+	}
+
 	file, err := os.Open("channels.json")
 	if err != nil {
 		log.Fatal(err)
