78371d91c8d7e1735acdb1750ade818c50b6d059
hdonapatiCBS <hareendra.Donapati@cbsinteractive.com>
2024-11-20 10:50:13 -0600

    xmltv refactor

--- routes/xmltv.go
+++ routes/xmltv.go
@@ -27,17 +27,12 @@ func XMLTV(c *gin.Context) {
 	var channels []ChannelSimplified
 
 	for index, channel := range config.Channels {
-		var icon *string
-        if channel.Icon != nil {
-            icon = channel.Icon
-        }
 		channels = append(
 			channels,
 			ChannelSimplified{
 				ID:   index + 1,
 				Name: channel.Name,
-				Icon: icon,
-			},
+			}
 		)
 	}
 
